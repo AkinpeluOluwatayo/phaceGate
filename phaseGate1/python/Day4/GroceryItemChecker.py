7f2fa486654710921f3fa46ae7fe25bd895da2fe
Akinpelu Oluwatayo <akinpeluoluwatayo1235@gmail.com>
2025-08-21 16:44:22 +0100

    ok

---
+++ phaseGate1/python/Day4/GroceryItemChecker.py
@@ -0,0 +1,46 @@
+def GroceryItemChecker ():
+	return addItem
+
+	while(true):
+		print('''
+
+		Welcome to Tayo Store
+
+	      1. Add items
+	      2. Remove items
+	      3. Show available items
+	      4. exit
+
+			''')
+
+	match:
+		case 1:
+			print('''
+			what items are you adding?
+		1. Cornflakes
+		2. Milk
+		3. butter
+		4. exit
+				''')
+
+			match:
+				case 1:
+					print('Cornflakes has been successfully added...')
+					break;
+				case 2:
+					print('Milk has been successfully added...')
+				case 3:
+					print('butter has been successfully added...')
+
+		case 2:
+			 print('''
+			what items are you removing?
+		1. Cornflakes
+		2. Milk
+		3. butter
+		4. exit
+					''');
+
+
+
+
